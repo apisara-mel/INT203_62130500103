8ef346e1cf83f2e0f7999a83cfdd7e6f28c315a3
Apisara Ngakor <apisara.99@mail.kmutt.ac.th>
2021-02-03 12:37:29 +0700

    [UPDATE]G2

--- 62130500103_groupwork_2/main.js
+++ 62130500103_groupwork_2/main.js
@@ -1,9 +1,16 @@
 const app = {
     data() {
         return {
-            msg: 'Hello, Vue3'
+            msg: 'Hello, Vue3',
+            shown: true,
+            url: 'http://www.sit.ac.th'
         }
-    }
-
+    },
+    created(){​​​​
+        console.log('message is '+this.msg)
+        }​​​​,  
+        updated() {​​​​
+        console.log('message is chaged to '+this.msg)
+        }​​​​
 }
-Vue.createApp(app).mount('#app')
+var mountedApp = Vue.createApp(app).mount('#app')
---
+++ 62130500103_groupwork_2/profile.js
@@ -0,0 +1,13 @@
+const app ={
+    data(){
+        return{
+            name: 'Apisara Ngakor',
+            tag: '@CARAMEL_XM99',
+            articles: '48',
+            followers: '1.5K',
+            following: '798'
+        }
+    }
+}
+
+var mountedApp = Vue.createApp(app).mount('#app')
